d4b779e06ac95514478c9d0a64dbe6bd40919c05
Nataša Andžić <natasa.andzic@yahoo.com>
2018-10-28 22:54:43 +0100

    E-mail Intent added. Made some styles for headers (styles.xml)

--- app/src/main/java/com/example/natasaandzic/justjava/MainActivity.java
+++ app/src/main/java/com/example/natasaandzic/justjava/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.natasaandzic.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,63 +17,81 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffees=0;
+    int number=0;
+
+    private EditText nameEt;
+	private CheckBox soyMilkCb;
+	private CheckBox chocolateCb;
+	private TextView numberOfCoffeesTv;
+
+	private Button incrementBtn;
+	private Button decrementBtn;
+
+	private TextView totalPriceTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	    nameEt = (EditText)findViewById(R.id.name);
+
+	    soyMilkCb = (CheckBox)findViewById(R.id.soyMilkCheckbox);
+	    chocolateCb = (CheckBox)findViewById(R.id.chocolateCheckbox);
+	    numberOfCoffeesTv = (TextView)findViewById(R.id.quantityNumber);
+
+	    incrementBtn = (Button)findViewById(R.id.inc);
+	    decrementBtn = (Button)findViewById(R.id.dec);
+
+	    totalPriceTv = findViewById(R.id.orderSummaryNumber);
+
+	    incrementBtn.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+			    number++;
+			    if(number>30) {
+				    Toast.makeText(MainActivity.this, "You cannot have more than 30 cups of coffee", Toast.LENGTH_SHORT).show();
+				    number=30; }
+			    numberOfCoffeesTv.setText("" + number);
+		    }
+	    });
+
+	    decrementBtn.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+			    if (number > 0) {
+				    Toast.makeText(MainActivity.this, "You cannot have less than 0 cups of coffee", Toast.LENGTH_SHORT).show();
+				    number--;
+				    numberOfCoffeesTv.setText("" +number);
+			    } else {
+				    number = 0;
+				    numberOfCoffeesTv.setText("" +number);
+			    }
+		    }
+	    });
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       // String message = "Total: " +numberOfCoffees*190+ "RSD";
-       // displayMessage(message);
-        TextView name = findViewById(R.id.name);
-        String value = name.getText().toString();
-        CheckBox soyMilk = findViewById(R.id.soyMilkCheckbox);
-        boolean hasSoyMilk = soyMilk.isChecked();
-        CheckBox chocolate = findViewById(R.id.chocolateCheckbox);
-        boolean hasChocolate = chocolate.isChecked();
+        String value = nameEt.getText().toString();
+        boolean hasSoyMilk = soyMilkCb.isChecked();
+        boolean hasChocolate = chocolateCb.isChecked();
         int price = calculatePrice(hasSoyMilk, hasChocolate);
         String fullMessage = createOrderSummary(price, hasSoyMilk, hasChocolate, value);
-        displayMessage(fullMessage);
+        totalPriceTv.setText(fullMessage);
 
-       // Intent intent = new Intent(Intent.ACTION_SENDTO);
-      //  intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        // intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " +name);
-        //intent.putExtra(Intent.EXTRA_TEXT, "Just Java order for " +name);
-        //if (intent.resolveActivity(getPackageManager()) != null) {
-          //  startActivity(intent);
-           // }
-      //  }
-    }
+       Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " +value);
+        intent.putExtra(Intent.EXTRA_TEXT, "Just Java order for " +fullMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+          startActivity(intent);
+            }
+       }
 
-    private void displayPrice(int number) {
-        TextView ukupnaCena =  findViewById(R.id.orderSummaryNumber);
-        ukupnaCena.setText("" + number);
-    }
 
-    public void increment(View view) {
-        numberOfCoffees++;
-        if(numberOfCoffees>30) {
-            Toast.makeText(this, "You cannot have more than 30 cups of coffee", Toast.LENGTH_SHORT).show();
-            numberOfCoffees=30; }
-        display(numberOfCoffees);
-    }
-
-    public void decrement(View view) {
-        if (numberOfCoffees > 0) {
-            Toast.makeText(this, "You cannot have less than 0 cups of coffee", Toast.LENGTH_SHORT).show();
-            numberOfCoffees--;
-            display(numberOfCoffees);
-        } else {
-            numberOfCoffees = 0;
-            display(numberOfCoffees);
-        }
-    }
 
     private int calculatePrice(boolean soy, boolean choco) {
         int basePrice=190;
@@ -77,28 +99,17 @@ public class MainActivity extends AppCompatActivity {
             basePrice=basePrice + 30;
         if(choco==true)
             basePrice=basePrice + 50;
-        return numberOfCoffees * basePrice;
+        return number * basePrice;
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView numberOfCoffees = (TextView) findViewById(R.id.quantityNumber);
-        numberOfCoffees.setText("" + number);
-    }
-
-    private void displayMessage(String m) {
-        TextView message = findViewById(R.id.orderSummaryNumber);
-        message.setText(m);
-    }
 
     private String createOrderSummary(int price, boolean soy, boolean choco, String name) {
+    	String quantity = numberOfCoffeesTv.getText().toString();
         String priceMessage = "Name " +name;
-        priceMessage+= "\nQuantity: " +numberOfCoffees;
+        priceMessage+= "\nQuantity: " +quantity;
         priceMessage+= "\nSoy milk? " +soy;
         priceMessage+="\nChocolate? " +choco;
-        priceMessage+="\nTotal: " +numberOfCoffees*price + " RSD";
+        priceMessage+="\nTotal: " +number*price + " RSD";
         priceMessage+="\nThank you! :)";
         return priceMessage;
 
